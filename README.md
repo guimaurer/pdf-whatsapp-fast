d22c5e5da277bb0211bf4a351a4f45f518c17233
Guilherme Maurer <guilhermemaurer@hotmail.com>
2022-12-30 00:32:28 +0000

    commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# pdf-whatsapp-fast
-A whatsapp bot that helps you with PDFs. Merge, compress and convert PDFs with just a few messages. 100% FREE
--- database/db.js
+++
@@ -1,7 +0,0 @@
-const Sequelize = require('sequelize');
-const sequelize = new Sequelize({
-    dialect: 'sqlite',
-    storage: '/workspace/pdf-whatsapp-fast/database/database.sqlite'
-  })
- 
-module.exports = sequelize;
